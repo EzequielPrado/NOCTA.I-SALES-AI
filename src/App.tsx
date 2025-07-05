3eb37e267c41bf3b3af89d0fc80a0cbab748d16e
Ezequiel Reis <65899925+EzequielPrado@users.noreply.github.com>
2025-07-04 23:50:46 -0300

    Logo png

--- src/App.tsx
+++ src/App.tsx
@@ -138,7 +138,7 @@ function App() {
                 className="flex items-center gap-3 hover:opacity-80 transition-opacity"
               >
                 <img 
-                  src="https://imgur.com/fxfSiHc" 
+                  src="https://imgur.com/fxfSiHc.png" 
                   alt="NOCTA.I Logo" 
                   className="h-8 w-auto"
                 />
